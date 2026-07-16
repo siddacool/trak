db0e0f264093d0f9994828d681e273401d47e0a0
Siddhesh Mangela <sidthecool007@gmail.com>
2026-07-16 15:39:11 +0530

    Changeset init

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,56 @@
 <script lang="ts">
-  import favicon from '$lib/assets/favicon.svg';
+  import '@flightlesslabs/dodo-ui/styles/global/index.scss';
+  import '@flightlesslabs/dodo-ui/styles/components.scss';
+  // Addon (dodo-ui-bits)
+  import '@flightlesslabs/dodo-ui-bits/styles/main.scss';
+  // Addon (dodo-ui-date)
+  import '@flightlesslabs/dodo-ui-date/styles/main.css';
+
+  import { ModalManager, ToastManager } from '@flightlesslabs/dodo-ui-bits';
+  import { Theme } from '@flightlesslabs/dodo-ui';
 
   let { children } = $props();
 </script>
 
-<svelte:head>
-  <link rel="icon" href={favicon} />
-</svelte:head>
+<Theme global class="Theme" type="auto">
+  <div class="App">
+    {@render children()}
+  </div>
 
-{@render children()}
+  <ToastManager clearAfterDuration={3000} alignmentY="bottom" alignmentX="center" limit={3} />
+  <ModalManager />
+</Theme>
+
+<style lang="scss">
+  :global(*) {
+    box-sizing: border-box;
+  }
+
+  :global(html) {
+    font-family: sans-serif;
+    font-optical-sizing: auto;
+    font-size: 16px;
+  }
+
+  :global(body) {
+    margin: 0;
+    height: 100vh;
+    overflow: hidden;
+  }
+
+  :global(.Theme) {
+    height: 100vh;
+    overflow: hidden;
+  }
+
+  .App {
+    display: flex;
+    height: 100%;
+    width: 100%;
+    flex-direction: column;
+  }
+
+  :global(.dodo-theme--dark) {
+    color-scheme: dark;
+  }
+</style>
